ee8e5496599c5e8136a6d38ce1f8b6fd4953a485
Pushpa <pushpa01082020@gmail.com>
2026-01-19 12:21:26 +0530

    Fix waste app module path for production imports

--- backend/smartwaste_backend/waste/apps.py
+++ backend/smartwaste_backend/waste/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class WasteapiConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'waste'
+    name = 'smartwaste_backend.waste'
