8b4b4d7eb17cfd8feab4636c59f5e8290a38d727
Pushpa <pushpa01082020@gmail.com>
2026-01-19 15:35:02 +0530

    Add render start script

--- backend/smartwaste_backend/smartwaste_backend/settings.py
+++ backend/smartwaste_backend/smartwaste_backend/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 from django.urls import reverse_lazy
+import os
+from urllib.parse import urlparse
 
 LOGIN_URL = reverse_lazy('admin:login')
 
@@ -27,9 +29,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-eypp%g(!lye_8*5wmbyhl(pl)qoy1lx+3u(p56+6g%=yfkf87q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = ["*"]   
+ALLOWED_HOSTS = ["smart-waste-tracker.onrender.com", "localhost", "127.0.0.1"]   
 
 
 # Application definition
@@ -103,12 +105,29 @@ WSGI_APPLICATION = 'smartwaste_backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+DATABASE_URL = os.environ.get("DATABASE_URL")
+
+if DATABASE_URL:
+    url = urlparse(DATABASE_URL)
+
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": url.path[1:],  # Remove leading /
+            "USER": url.username,
+            "PASSWORD": url.password,
+            "HOST": url.hostname,
+            "PORT": url.port,
+        }
     }
-}
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
+
 
 
 # Password validation
